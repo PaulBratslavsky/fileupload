81e935f7f52637d9ddd8f87aff6a6c0c9ca000d5
paul bratslavsky <paulbratslavsky@pauls-MBP.home>
2022-03-23 23:29:15 -0400

    updated the uploadFilesController in content api

--- src/extensions/upload/strapi-server.js
+++ src/extensions/upload/strapi-server.js
@@ -1,7 +1,16 @@
 const validateUploadBody = require("./validation/upload");
+const utils = require('@strapi/utils');
+const { sanitize } = utils;
 
 const getService = (name) => {
   return strapi.plugin("upload").service(name);
+};
+
+const sanitizeOutput = (data, ctx) => {
+  const schema = strapi.getModel('plugin::upload.file');
+  const { auth } = ctx.state;
+
+  return sanitize.contentAPI.output(data, schema, { auth });
 };
 
 const ACTIONS = {
@@ -17,6 +26,7 @@ const fileModel = "plugin::upload.file";
 
 module.exports = (coreApi) => {
 
+  console.log(coreApi.controllers["content-api"].uploadFiles, "heh")
   coreApi.controllers["admin-api"].uploadFiles = async (ctx) => {
     const {
       state: { userAbility, user },
@@ -61,6 +71,41 @@ module.exports = (coreApi) => {
     ).catch((err) => console.log(err, "Failed to create document"));
 
     ctx.body = response;
+  };
+
+  coreApi.controllers["content-api"].uploadFiles = async (ctx) => {
+    const {
+      request: { body, files: { files } = {} },
+    } = ctx;
+
+    const { user } = ctx.state;
+
+    const uploadedFiles = await getService("upload").upload({
+      data: await validateUploadBody(body),
+      files,
+    });
+
+
+    Promise.all(
+      uploadedFiles.map(async (item) => {
+        const imageData = {
+          name: item.name,
+          ext: item.ext,
+          mime: item.mime,
+          url: item.url,
+          provider: item.provider,
+          image: item.id,
+          imageID: item.id.toString(),
+          uploadedBy: user ? user.id : null,
+        };
+
+        await strapi.service("api::document.document").create({
+          data: { ...imageData },
+        });
+      })
+    ).catch((err) => console.log(err, "Failed to create document"));
+
+    ctx.body = await sanitizeOutput(uploadedFiles, ctx);
   };
 
   return coreApi;
